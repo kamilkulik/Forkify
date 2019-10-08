958f5754d49a471b24feddc4f653244bbd4e94fc
Kamil <37276273+kamilkulik@users.noreply.github.com>
2019-10-08 21:48:17 +0200

    Axios installed

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,7 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+async function getResults(query) {
+    
+}
+getResults();
+
+// https://www.food2fork.com/api/search
+// API Key: 6198ba1731f65498c4f203efc5131cf6
